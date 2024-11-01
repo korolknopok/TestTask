478ce76c1cd400610800a0d2a51506159f782763
korolknopok <fedor.artem.ivanov@mail.ru>
2024-11-01 12:26:13 +0700

    chore: setup project with Webpack and TypeScript

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+        })
+    ],
+    devServer: {
+        static: './dist',
+        port: 3000,
+        hot: true,
+    },
+    mode: 'development',
+}
